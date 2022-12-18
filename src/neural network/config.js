80df2568828e8a9600b493ac4a4e61a2d465af12
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2022-12-18 01:35:35 +0100

    Add config file for debug and other stuff

--- src/neural network/config.js
+++ src/neural network/config.js
@@ -4,13 +4,14 @@ const training_data = [
     // NOTE : The order of the data is important
     // ? [LEFT, RIGHT, UP, DOWN]
     new DataModel([1, 0, 3, 0], [LEFT, UP]),
-    
+
 
 ];
+// Generate some random data
 for (let index = 0; index < 20; index++) {
     training_data.push(randomData());
 }
-console.table(training_data[0].get())
 network.train(training_data);
 var test = network.run([0, 0, 11, 10]);
-console.info(predictedKey(test));
+debug.table(training_data)
+debug.info(predictedKey(test));
